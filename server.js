d78630d8abcf6057e1d7d38da6143eede3ef667d
sajalsinghal2000 <b241226@skit.ac.in>
2026-03-04 11:49:42 +0530

    logun

--- server.js
+++ server.js
@@ -589,7 +589,7 @@ app.get('/api/admin/billing', async (req, res) => {
    START SERVER
 ========================= */
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
