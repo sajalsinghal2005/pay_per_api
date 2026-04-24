db00413acc136b82f6bb9735d77dedbe0c4f8aa6
sajalsinghal2000 <b241226@skit.ac.in>
2026-04-24 23:45:07 +0530

    fix mongo uri fallback

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectMongo = async () => {
-  const uri = process.env.MONGODB_URI;
+  const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://sajal:sajal12345@cluster0.acw0pev.mongodb.net/payperapi?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI)
   if (!uri) {
     throw new Error('MONGODB_URI is required in .env');
   }
